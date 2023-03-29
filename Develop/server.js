b3c3a2bf191898cfe44c37c5679e44c46d56ffd6
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-28 21:13:38 -0700

    added files

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequalize= require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequalize.sync({force:false}).then(()=>{
+  console.log ("We are connected to database!")
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+})
+
